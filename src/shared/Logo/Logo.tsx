925c2d72ba452f58d45ad8a49df25948d782ef1e
el-wiam <elfardwiam@gmail.com>
2024-07-23 13:01:01 +0100

    logo

--- src/shared/Logo/Logo.tsx
+++ src/shared/Logo/Logo.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import type { FC } from 'react';
 import React from 'react';
-import { RiMicrosoftLoopFill } from 'react-icons/ri';
+// import { RiMicrosoftLoopFill } from 'react-icons/ri';
 
 interface LogoProps {
   className?: string;
@@ -10,7 +10,7 @@ interface LogoProps {
 const Logo: FC<LogoProps> = ({ className = 'hidden' }) => {
   return (
     <Link className="flex cursor-pointer items-center gap-2" href="/">
-      <RiMicrosoftLoopFill className="text-3xl text-primary" />{' '}
+      {/* <RiMicrosoftLoopFill className="text-3xl text-primary" />{' '} */}
       <span className={`${className} text-2xl font-bold`}>HotKicks.</span>
     </Link>
   );
